847cc7a4bd646b4908f5f9d7dfc2628588a54c9e
airentech <airentech.co@gmail.com>
2025-07-05 04:00:57 -0500

    Initial Commit

---
+++ MonitorClient/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MonitorClient
+//
+//  Created by Uranus on 7/5/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ MonitorClientTests/MonitorClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  MonitorClientTests.swift
+//  MonitorClientTests
+//
+//  Created by Uranus on 7/5/25.
+//
+
+import Testing
+@testable import MonitorClient
+
+struct MonitorClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
